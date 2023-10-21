210b02b5025c2e3a287a3fdd440c4c4bca6b767d
Jáhìd hassan <140346514+SKBER-CYBER@users.noreply.github.com>
2023-10-22 01:38:27 +0600

    Update Lover.py

--- Lover.py
+++ Lover.py
@@ -1,3 +1,5 @@
+#বেশারবেটা Dec করবু তোর বোনকে গুহা মারমু লটিরবেটা
+
 import os,platform
 os.system('clear')
 print('[•] Checking Updates...')
@@ -5,7 +7,7 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
-    os.system('xdg-open https://facebook.com/groups/790381136153614/')
-    import V5
+    os.system('xdg-open https://chat.whatsapp.com/E58X2PpvK19Io8fRYRzZzK')
+    import Jahid
 else:
     exit('\033[1;31m[×] Sorry Device Not Support')
